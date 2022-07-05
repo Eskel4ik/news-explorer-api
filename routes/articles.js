6465d752beccc1e30f9b45399a5db53af2eb9565
OlegTabachnikow <eskelll4ik@gmail.com>
2022-07-05 21:05:15 +0300

    hotfix joi validation of auth

--- routes/articles.js
+++ routes/articles.js
@@ -1,16 +1,8 @@
 const articlesRouter = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
-const validator = require('validator');
+
 const {
   getArticles, createArticle, deleteArticle,
 } = require('../controllers/articles');
-
-const validateURL = (value, helpers) => {
-  if (validator.isURL(value)) {
-    return value;
-  }
-  return helpers.error('string.uri');
-};
 
 articlesRouter.get('/articles', getArticles);
 
